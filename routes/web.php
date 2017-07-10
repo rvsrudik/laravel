c3c8945463a0a7a013f04934a1f63cf2cccc8403
Vitalii Rudenko <vrudenko@e1r1p15.unit.ua>
2017-07-10 11:12:58 +0300

    1. tested routes with get/post request. 2. excluded 'comments' from CSRF protection

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/page', function () {
+    echo "hello";
+    return;
+});
+
+Route::get('/env', function ()
+{
+   echo '<pre>';
+   print_r($_ENV);
+   echo '</pre>';
+
+//   echo config('app.locale');
+//   Config::set('app.locale', 'ru');
+//   echo Config::get('app.locale');
+
+   echo env('APP_ENV');
+});
+
+//Route::post('/comments', function () {
+//    print_r($_POST);
+//});
+//
+//Route::match(['get', 'post'], '/comments', function () {
+//  print_r($_POST);
+//
+//});
+
+//Route::any('/comments', function () {
+//    print_r($_POST);
+//
+//});
