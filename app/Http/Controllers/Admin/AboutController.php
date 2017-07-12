c06b1b9a1405db964c3ea3fd2e02d01d6456e386
Vitalii Rudenko <vrudenko@e1r1p13.unit.ua>
2017-07-12 15:01:44 +0300

    work with request

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Auth\Access\Response;
+//use Illuminate\Auth\Access\Response;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+//use Illuminate\Http\Response;
 use Illuminate\Http\Response;
+
 
 class AboutController extends Controller
 {
@@ -14,10 +17,36 @@ class AboutController extends Controller
     public function show()
     {
 
-        return 'hello';
         if (view()->exists('default.about')) {
 
-//            return (new Response('hello', 200, array()));
+
+            $view = view('default.about')->withTitle('Hello World')->render();
+//            return (new Response($view))->header('Content-Type', 'newType')->header('Header-One', 'headerONE');
+
+//            return response($view)->header('Content-Type', 'newType');
+
+//            return response()->json(['name'=>'Hello', 'namr1'=>'Hello1']);
+
+//            return response()->view('default.about', ['title'=>'Hello world']);
+
+//            return response()->download('robots.txt', 'mytext.txt');
+
+//             return response($view)->withHeaders([
+//                                            'headerOne'=>'hello',
+//                                            'headerTwo'=>'hello2',
+//             ]);
+
+//            return redirect('/');
+
+//            return redirect()->route('home');
+
+//            return redirect()->action('Admin\ContactController@show');
+
+            return redirect('/articles')->with('param1', 'Hello');
+
+//            return new RedirectResponse('/articles');
+
+//            return RedirectResponse::create('/articles');
 
 //            return view('default.about')->withTitle('Hello World');
         }
